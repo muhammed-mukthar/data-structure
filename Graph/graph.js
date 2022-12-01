5e485764803bd31b1e11447a1c959ba9e0033522
Muhammed Mukthar <muktharmanaf28@gmail.com>
2022-12-01 11:33:02 +0530

    completed insertion Sort

--- Graph/graph.js
+++ Graph/graph.js
@@ -8,42 +8,42 @@ class Graph {
       this.adjacencyList[vertex] = new Set();
     }
   }
-  addEdge(vertex1,vertex2){
-    if(!this.adjacencyList[vertex1]){
-        this.addVertex(vertex1)
+  addEdge(vertex1, vertex2) {
+    if (!this.adjacencyList[vertex1]) {
+      this.addVertex(vertex1);
     }
-    if(!this.adjacencyList[vertex2]){
-        this.addVertex(vertex2)
+    if (!this.adjacencyList[vertex2]) {
+      this.addVertex(vertex2);
     }
-    this.adjacencyList[vertex1].add(vertex2)
+    this.adjacencyList[vertex1].add(vertex2);
 
-    this.adjacencyList[vertex2].add(vertex1)
+    this.adjacencyList[vertex2].add(vertex1);
   }
-  removeVertex(vertex){
-    if(!this.adjacencyList[vertex]){
-        return
+  removeVertex(vertex) {
+    if (!this.adjacencyList[vertex]) {
+      return;
     }
-    for(let adjacentVertex of this.adjacencyList[vertex]){
-        this.removeEdge(vertex,adjacentVertex)
+    for (let adjacentVertex of this.adjacencyList[vertex]) {
+      this.removeEdge(vertex, adjacentVertex);
     }
-    delete this.adjacencyList[vertex]
-  }
-
-  removeEdge(vertex1,vertex2){
-    this.adjacencyList[vertex1].delete(vertex2)
-    this.adjacencyList[vertex2].delete(vertex1)
+    delete this.adjacencyList[vertex];
   }
 
-  display(){
-    for(let vertex in this.adjacencyList){
-        console.log(vertex+"->"+[...this.adjacencyList[vertex]]);
+  removeEdge(vertex1, vertex2) {
+    this.adjacencyList[vertex1].delete(vertex2);
+    this.adjacencyList[vertex2].delete(vertex1);
+  }
+
+  display() {
+    for (let vertex in this.adjacencyList) {
+      console.log(vertex + "->" + [...this.adjacencyList[vertex]]);
     }
   }
-  hasEdge(vertex1,vertex2){
+  hasEdge(vertex1, vertex2) {
     return (
-        this.adjacencyList[vertex1].has(vertex2)&&
-        this.adjacencyList[vertex2].has(vertex1)
-    )
+      this.adjacencyList[vertex1].has(vertex2) &&
+      this.adjacencyList[vertex2].has(vertex1)
+    );
   }
 }
 
@@ -52,10 +52,10 @@ const graph = new Graph();
 graph.addVertex("A");
 graph.addVertex("B");
 graph.addVertex("C");
-graph.addEdge("A","B")
-graph.addEdge('B','C')
-graph.display()
-console.log(graph.hasEdge('A','C'));
-graph.removeVertex('B')
+graph.addEdge("A", "B");
+graph.addEdge("B", "C");
+graph.display();
+console.log(graph.hasEdge("A", "C"));
+graph.removeVertex("B");
 
-graph.display()
+graph.display();
